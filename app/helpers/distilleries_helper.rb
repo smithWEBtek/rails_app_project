251554f504c65864ccf908d603ec5cd549da134a
lauraBo <BodellLaura@gmail.com>
2018-08-14 13:18:11 -0400

    created a distilleries helper file in helpers. created 2 methods in the file to help display data in bourbon view files

---
+++ app/helpers/distilleries_helper.rb
@@ -0,0 +1,16 @@
+module DistilleriesHelper
+
+
+  def display_distillery(bourbon)
+  bourbon.distillery.nil? ? link_to("Add Distillery", edit_bourbon_path(bourbon)) : link_to(bourbon.distillery_name, distillery_path(bourbon.distillery))
+  end
+
+  def distillery_select(distillery, bourbon)
+    if distillery
+      distillery.name
+    else
+      select_tag "bourbon[distillery_id]", options_from_collection_for_select(Distillery.all, :id, :name)
+    end
+  end
+  
+end
