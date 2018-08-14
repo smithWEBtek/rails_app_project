119884d8dddabe6c3c8a5c7a7987130321bb3725
lauraBo <BodellLaura@gmail.com>
2018-08-14 17:39:22 -0400

    created Stockists Controller with all CRUD actions. added a private method for stockist params to include bourbon attributes. added accepts nest attribute for in Stockist model file

--- app/controllers/stockists_controller.rb
+++ app/controllers/stockists_controller.rb
@@ -1,2 +1,61 @@
 class StockistsController < ApplicationController
+
+  def index
+    @stockists = Stockist.all
+  end
+
+  def show
+    @stockist = Stockist.find(params[:id])
+  end
+
+  def new
+    @stockist = Stockist.new
+  end
+
+  def create
+    @stockist = Stockist.new(stockist_params)
+    if @stockist.save
+      redirect_to @stockist
+    else
+     render :new
+   end
+  end
+
+  def edit
+    @stockist = Stockist.find(params[:id])
+  end
+
+  def update
+    @stockist = Stockist.find(params[:id])
+    @stockist.update(stockist_params)
+    if @stockist.save
+      redirect_to @stockist
+    else
+      render :edit
+     end
+  end
+
+  def destroy
+    @stockist = Stockist.find(params[:id])
+    @stockist.destroy
+    flash[:notice] = "Stockist deleted"
+    redirect_to Stockists_path
+  end
+
+  private
+
+  def stockist_params
+    params.require(:stockist).permit(:name, :address, bourbon_ids:[], bourbon_attributes: [:name])
+  end
+
+  def bourbon_params
+    params.require(:bourbon).permit(  stockist_ids:[],
+    stockists_attributes: [:name])
+  end
+
+  end
+
+
+
+
 end
--- app/models/stockist.rb
+++ app/models/stockist.rb
@@ -2,6 +2,7 @@ class Stockist < ApplicationRecord
 
   has_many :bourbon_stockists
   has_many :bourbons, through: :bourbon_stockists
+  accepts_nested_attributes_for :bourbons
 
 
 end
