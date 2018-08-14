86e0aacf4735471a5fd03da070c807880a28c7fb
lauraBo <BodellLaura@gmail.com>
2018-08-14 12:45:50 -0400

    created destroy, update and private params method in Distilleries Controller

--- app/controllers/distilleries_controller.rb
+++ app/controllers/distilleries_controller.rb
@@ -1,2 +1,43 @@
 class DistilleriesController < ApplicationController
+
+def index
+  @distilleries = Distillery.all
+end
+
+def show
+end
+
+def new
+end
+
+def create
+end
+
+def edit
+end
+
+def update
+  @distillery = Distillery.find(params[:id])
+  @distillery.update(distillery_params)
+  if @distillery.save
+    redirect_to @distillery
+  else
+    render :edit
+   end
+end
+
+def destroy
+  @distillery = Distillery.find(params[:id])
+  @distillery.destroy
+  flash[:notice] = "Distillery deleted"
+  redirect_to distilleries_path
+end
+
+private
+
+def distillery_params
+  params.require(:distillery).permit(:name, :address)
+end
+
+
 end
