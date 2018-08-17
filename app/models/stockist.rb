4825e6ba806859461fde1f97c3cb8e02aa7ae836
lauraBo <BodellLaura@gmail.com>
2018-08-17 17:00:33 -0400

    added validation for presence of name in Stockist model

--- app/models/stockist.rb
+++ app/models/stockist.rb
@@ -2,7 +2,9 @@ class Stockist < ApplicationRecord
 
   has_many :bourbon_stockists
   has_many :bourbons, through: :bourbon_stockists
+
   accepts_nested_attributes_for :bourbons
+  validates :name, presence: true
 
 
 end
