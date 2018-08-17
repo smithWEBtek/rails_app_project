4b922ccb3b408268f35c514a3a1232716ff38178
lauraBo <BodellLaura@gmail.com>
2018-08-17 15:08:38 -0400

    added bourbon_stockist_params method to Bourbon_stockist_controller

--- app/controllers/bourbon_stockists_controller.rb
+++ app/controllers/bourbon_stockists_controller.rb
@@ -7,11 +7,16 @@ end
 def create
   @bourbon_stockist = Bourbon_Stockist.new(bourbon_stockist_params)
   if @bourbon_stockist.save
-    redirect_to @bourbon_stockist
-else
+    redirect_to @bourbon_stockist.bourbon
+   else
   render :new
-end
+   end
 end
 
+private
+
+def bourbon_stockist_params
+   params.require(:bourbon_stockist).permit(:notes, :bourbon_id, :stockist_id)
+end
 
 end
