294212df293ec9007d3c5fffc34f14a072eecede
lauraBo <BodellLaura@gmail.com>
2018-08-17 23:07:18 -0400

    created Update, Edit and Destroy methods in BourbonStockist Controller. Added view files for BourbonStockist - _form partial, and edit

--- app/controllers/bourbon_stockists_controller.rb
+++ app/controllers/bourbon_stockists_controller.rb
@@ -17,6 +17,31 @@ def index
   @bourbon_stockists = BourbonStockist.all
 end
 
+def edit
+  @bourbon_stockist = BourbonStockist.find(params[:id])
+end
+
+
+def update
+  @bourbon_stockist = BourbonStockist.find(params[:id])
+  @bourbon_stockist.update(bourbon_stockist_params)
+  if @bourbon_stockist.save
+    redirect_to @bourbon_stockist
+  else
+    render :edit
+   end
+end
+
+  def destroy
+    @bourbon_stockist = BourbonStockist.find(params[:id])
+    @bourbon_stockist.destroy
+    flash[:notice] = "Note deleted"
+    redirect_to bourbons_path
+  end
+
+
+
+
 
 private
 
