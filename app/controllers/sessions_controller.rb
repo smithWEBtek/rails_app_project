f526cd8bc7f660e41da6b2ef5ca474ddc52b1f41
lauraBo <BodellLaura@gmail.com>
2018-08-18 16:57:17 -0400

    created Sessions Controller with new, create and destoy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,23 @@
 class SessionsController < ApplicationController
+
+
+  def new
+  end
+
+  def create
+    user = User.find_by(username: params[:username])
+    user = user.try(:authenticate, params[:password])
+    return redirect_to(controller: 'sessions', action: 'new') unless user
+
+    session[:user_id] = user.id
+
+    @user = user
+    redirect_to controller: 'bourbons', action: 'index'
+  end
+
+  def destroy
+    session.delete :user_id
+    redirect_to controller: 'welcome', action: 'hello'
+  end
+
 end
