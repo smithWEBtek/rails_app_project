b861569bc59e934ca8be0d472b64f31a1ff77394
lauraBo <BodellLaura@gmail.com>
2018-08-17 22:50:47 -0400

    created stockist_name custom getter and setter method in BourbonStockist model

--- app/models/bourbon_stockist.rb
+++ app/models/bourbon_stockist.rb
@@ -4,6 +4,15 @@ class BourbonStockist < ApplicationRecord
   accepts_nested_attributes_for :stockist
 
   #validates :notes, presence: true
+  def stockist_name
+      #self.try(:distillery).try(:distillery)
+      #self.distillery.name
+      self.stockist ? self.stockist.name : nil
+    end
 
+    def stockist_name=(name)
+      stockist = Stockist.find_or_create_by(name: name)
+      self.stockist = stockist
+    end
 
 end
