c05adb2563a7c05542b8cdf7b40bcc326c1fd085
lauraBo <BodellLaura@gmail.com>
2018-08-19 18:54:23 -0400

    edited user welcome index page to include links to bourbon, stockist and distilleries index pages. changed user variable from local to instance in User create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,14 +6,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(username: params[:username])
-    user = user.try(:authenticate, params[:password])
-    return redirect_to(controller: 'sessions', action: 'new') unless user
+    @user = User.find_by(username: params[:username])
+    @user = @user.try(:authenticate, params[:password])
+    return redirect_to(controller: 'sessions', action: 'new') unless @user
 
-    session[:user_id] = user.id
+    session[:user_id] = @user.id
 
-    @user = user
-    redirect_to controller: 'bourbons', action: 'index'
+    #@user = user
+    redirect_to controller: 'welcome', action: 'home'
   end
 
   def destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
 has_secure_password
-validates :email, :presence => true
-validates :email, :uniqueness   => true
+
 validates :username, :presence => true
 validates :username, :uniqueness   => true
 end
