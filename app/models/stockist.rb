63662244ddf36f47f1b9d4da05126d22ed1aa7fc
lauraBo <BodellLaura@gmail.com>
2018-08-24 16:59:44 -0400

    removed before save method in Stockist model and added name validation instead

--- app/models/stockist.rb
+++ app/models/stockist.rb
@@ -3,7 +3,10 @@ class Stockist < ApplicationRecord
   has_many :bourbon_stockists
   has_many :bourbons, through: :bourbon_stockists
 
-  
+    validates_presence_of :name
+
+  #before_create :remove_stockists_without_name
+
 
   def bourbon_count
     self.bourbons.size
