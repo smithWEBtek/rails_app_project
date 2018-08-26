3b5acd0a1da7023ac8a5fdf384568db47aae525a
lauraBo <BodellLaura@gmail.com>
2018-08-26 11:47:51 -0400

    added after_save method to Stockist model to destroy records with blank attributes

--- app/models/bourbon.rb
+++ app/models/bourbon.rb
@@ -11,9 +11,48 @@ class Bourbon < ApplicationRecord
   scope :rare, -> { where("age = 25") }
 
 
-  accepts_nested_attributes_for :stockists,
-  :reject_if => proc { |att| att['name'].blank? },
-  :allow_destroy => true
+  accepts_nested_attributes_for :stockists, :reject_if => :reject_stockist, :allow_destroy => true
+
+
+
+   def reject_stockist(attributes)
+      if attributes[:name].blank? || attributes[:address].blank?
+        if attributes[:id].present?
+          attributes.merge!({:_destroy => 1}) && false
+        else
+          true
+        end
+      end
+    end 
+
+
+   #lambda { |a| a[:name].blank? || a[:address].blank? }, :allow_destroy => true
+
+
+  #:reject_if => :mark_stockists_for_destruction, :allow_destroy => true
+
+   #def mark_stockists_for_destruction
+     #stockists.each do |stockist|
+       #if stockist.name.blank? or stockist.address.blank?
+         #stockist.mark_for_destruction
+       #end
+     #end
+   #end
+
+  #def reject_stockist(attributes)
+    #exists = attributes['id'].present?
+    #empty = attributes.reject{|k,v| k == 'id'}.values.all?(&:blank?)
+    #attributes.merge!({:_destroy => 1}) if exists and empty # destroy empty stockist
+    #return (!exists and empty) # reject empty attributes
+  #end
+
+
+  #REJECT_ALL_BLANK_PROC	=	proc { |stockists_attributes| stockists_attributes.all? { |key, value| key == "_destroy" || value.blank? } }
+
+  #reject_if: proc
+  #{ |stockists_attributes| stockists_attributes['name'].blank? }
+
+  #=	proc { |attributes| attributes.all? { |key, value| key == "_destroy" || value.blank? } }
 
   #before_save :mark_children_for_removal
   #reject_if: :name_blank, allow_destroy: true
@@ -21,12 +60,6 @@ class Bourbon < ApplicationRecord
     #def name_blank(a)
       #valid? && a[:id].blank? && a[:value].blank?
     #end
-
-    #def self.rare
-      #where("age: 25")
-    #end
-
-
 
 #def mark_children_for_removal
   #stockists.each do |stockist|
--- app/models/stockist.rb
+++ app/models/stockist.rb
@@ -4,13 +4,21 @@ class Stockist < ApplicationRecord
   has_many :bourbons, through: :bourbon_stockists
 
 
+    after_save { |stockist| stockist.destroy if stockist.name.blank? }
+
 
   #before_create :remove_stockists_without_name
+
+  #before_save :destroy_if_blank
 
 
   def bourbon_count
     self.bourbons.size
   end
 
+  #private
+  #def destroy_if_blank
+    #logger.info "destroy_if_blank called"
+  #end
 
 end
