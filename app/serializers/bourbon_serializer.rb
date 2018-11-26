951210ae2be2eee46ac82ed2a53e3be3f14e8368
lauraBo <BodellLaura@gmail.com>
2018-11-26 16:30:34 -0500

    removed bourbon serializer that I created manually and generated new active record serializer.

--- app/serializers/bourbon_serializer.rb
+++ app/serializers/bourbon_serializer.rb
@@ -1,20 +1,3 @@
-class BourbonSerializer
-  def self.serialize(bourbon)
-
-    # start with the open brace to create a valid JSON object
-    serialized_bourbon = '{'
-
-    serialized_bourbon += '"id": ' + bourbon.id.to_s + ', '
-
-    serialized_bourbon += '"name": "' + bourbon.name + '", '
-    serialized_bourbon += '"age": ' + bourbon.age.to_s + ', '
-    serialized_bourbon += '"grain": "' + bourbon.grain + '", '
-    serialized_bourbon += '"description": "' + bourbon.description + '", '
-    # the author association can also be represented in JSON
-    serialized_bourbon += '"distillery": {'
-    serialized_bourbon += '"name": "' + bourbon.distillery.name + '"}'
-
-    # and end with the close brace
-    serialized_bourbon += '}'
-  end
+class BourbonSerializer < ActiveModel::Serializer
+  attributes :id, :name, :age, :grain, :description 
 end
