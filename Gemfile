edba83afa6280e296b6f6a9182b69314d1fcef70
lauraBo <BodellLaura@gmail.com>
2018-12-01 17:31:35 -0500

    added handlebars template to bottom of stockists index view page. required handlebars in manifest js file. installed handlebars gem in gemfile

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ gem 'jbuilder', '~> 2.5'
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
 
+gem 'handlebars-source', '~> 4.0.12'
 gem 'active_model_serializers'
 # Use jquery as the JavaScript library
 
--- app/assets/javascripts/stockists.js
+++ app/assets/javascripts/stockists.js
@@ -5,11 +5,15 @@ this.address = attributes.address;
 }
 
 $(function(){
-Item.templateSource = $(“#item-template”).html()
-Item.template = Handlebars.compile(Item.templateSource);
+Stockist.templateSource = $('#stockist-template').html()
+Stockist.template = Handlebars.compile(Stockist.templateSource);
 })
 
-Item.prototype.renderLI()
+
+Stockist.prototype.renderLI = function(){
+return Stockist.template(this)
+}
+
 
 
 
@@ -31,7 +35,7 @@ method: 'POST'
 .success(function(json){
   var newstockist = new Stockist(json);
   var stockistLi = newstockist.renderLI()
-$("ul.new-stockist")append(stockistLi)
+$('ul.new-stockist').append(stockistLi)
 })
 .error(function(response){
 console.log('you broke it?')
